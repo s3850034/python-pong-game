56367c0022d0df9e78a048a4ffb4f02b4d0f330d
Muhib Hasan <s3850034@student.rmit.edu.au>
2021-09-30 23:13:31 +1000

    Finishing the Pong Game

---
+++ paddle.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, coordinates):
+        super().__init__()
+        self.create_paddle()
+        self.goto(coordinates)
+
+    def create_paddle(self):
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
